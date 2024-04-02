94bae43b3e5517db0dc2f73e79f4d8f888e4693a
jiyoung_im <kuness@naver.com>
2024-04-02 17:22:42 +0900

    first_commit

---
+++ src/main/java/EarlyBird/EarlyBird/EarlyBirdApplication.java
@@ -0,0 +1,13 @@
+package EarlyBird.EarlyBird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyBirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlyBirdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EarlyBird/EarlyBird/EarlyBirdApplicationTests.java
@@ -0,0 +1,13 @@
+package EarlyBird.EarlyBird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlyBirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
